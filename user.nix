f93e854892a98e4342a9a94dadd255fe8eabe68a
Alistair Porter <alistair_porter@outlook.com>
2024-02-08 19:47:08 +0000

    fleek: apply

--- user.nix
+++ user.nix
@@ -41,6 +41,7 @@
         nnoremap <C-n> :NERDTree<CR>
         nnoremap <C-t> :NERDTreeToggle<CR>
         nnoremap <C-f> :NERDTreeFind<CR>
+        let NERDTreeShowHidden=1
       '';
     };
 #    nano = {
