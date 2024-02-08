3a96aa5510fa0fb1bcc3ed881e7cb755bc97b2d3
Alistair Porter <alistair_porter@outlook.com>
2024-02-08 18:39:50 +0000

    fleek: apply

--- user.nix
+++ user.nix
@@ -28,6 +28,25 @@
       enable = true;
       enableZshIntegration = true;
     };
+    nano = {
+      enable = true;
+      nanorc = ''
+        set atblanks
+        set autoindent
+        set constantshow
+        set cutfromcursor
+        set indicator
+        set linenumbers
+        set minibar
+        set showcursor
+        set softwrap
+        set speller "aspell -x -c"
+        set trimblanks
+        set whitespace "»·"
+        set zap
+        set multibuffer
+      '';
+    };
   };
  
 }
