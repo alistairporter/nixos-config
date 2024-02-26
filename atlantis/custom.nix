67746de8085c9be0f1e7f24647dcd45d2e62cca3
Alistair Porter <alistair_porter@outlook.com>
2024-02-26 15:47:58 +0000

    fleek: apply

--- atlantis/custom.nix
+++ atlantis/custom.nix
@@ -1,6 +1,6 @@
 { pkgs, misc, ... }: {
   # FEEL FREE TO EDIT: This file is NOT managed by fleek. 
-  home-manager.options.home.session.Path =
+  options.home.session.Path =
   [
     "$HOME/.cargo/bin"
     "$HOME/.local/bin"
