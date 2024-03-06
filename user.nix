08e08a6cc45acd67f9bf376f988a1ca05b968f30
Alistair Porter <alistair_porter@outlook.com>
2024-03-06 01:44:57 +0000

    fleek: apply

--- user.nix
+++ user.nix
@@ -115,25 +115,28 @@
         let NERDTreeShowHidden=1
       '';
     };
-#    nano = {
-#      enable = true;
-#      nanorc = ''
-#        set atblanks
-#        set autoindent
-#        set constantshow
-#        set cutfromcursor
-#        set indicator
-#        set linenumbers
-#        set minibar
-#        set showcursor
-#        set softwrap
-#        set speller "aspell -x -c"
-#        set trimblanks
-#        set whitespace "»·"
-#        set zap
-#        set multibuffer
-#      '';
-#    };
   };
- 
+
+  home.file = {
+    ".nanorc" = {
+      enable = true;
+      target = "./";
+      text = ''
+        set atblanks
+        set autoindent
+        set constantshow
+        set cutfromcursor
+        set indicator
+        set linenumbers
+        set minibar
+        set showcursor
+        set softwrap
+        set speller "aspell -x -c"
+        set trimblanks
+        set whitespace "»·"
+        set zap
+        set multibuffer
+      '';
+    };
+  }; 
 }
