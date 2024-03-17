655833aa260a1d48e33816cf16f4a92fdb829f33
Alistair Porter <alistair_porter@outlook.com>
2024-03-17 16:55:44 +0000

    fleek: apply

--- user.nix
+++ user.nix
@@ -11,12 +11,12 @@
          "zsh-users/zsh-completions"
          "belak/zsh-utils path:completion"
          "ohmyzsh/ohmyzsh path:lib"
-         "ohmyzsh/ohmyzsh path:plugins/git"
-         "ohmyzsh/ohmyzsh path:plugins/lol"
+#         "ohmyzsh/ohmyzsh path:plugins/git"
+#         "ohmyzsh/ohmyzsh path:plugins/lol"
 #         "ohmyzsh/ohmyzsh path:plugins/common-aliases"
          "ohmyzsh/ohmyzsh path:plugins/sudo"
-         "ohmyzsh/ohmyzsh path:plugins/tmux"
-         "ohmyzsh/ohmyzsh path:plugins/git"
+#         "ohmyzsh/ohmyzsh path:plugins/tmux"
+#         "ohmyzsh/ohmyzsh path:plugins/git"
 #         "ohmyzsh/ohmyzsh path:plugins/docker"
         ];
       };
