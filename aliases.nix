864741e9861ebb240c7d62c32d7e8a2645fdfd3b
Alistair Porter <alistair_porter@outlook.com>
2024-03-27 14:42:52 +0000

    fleek: apply

--- aliases.nix
+++ aliases.nix
@@ -1,9 +1,21 @@
 { pkgs, misc, ... }: {
   # DO NOT EDIT: This file is managed by fleek. Manual changes will be overwritten.
    home.shellAliases = {
+    "cp" = "cp -i";
+    
+    "dud" = "du -d 1 -h";
+    
     "fleeks" = "cd ~/.local/share/fleek";
     
+    "l" = "ls -lh";
+    
     "latest-fleek-version" = "nix run https://getfleek.dev/latest.tar.gz -- version";
+    
+    "lsa" = "ls -lah";
+    
+    "mv" = "mv -i";
+    
+    "t" = "tail -f";
     
     "update-fleek" = "nix run https://getfleek.dev/latest.tar.gz -- update";
     };
--- olympus/custom.nix
+++ olympus/custom.nix
@@ -1,5 +1,7 @@
 { pkgs, misc, ... }: {
   # FEEL FREE TO EDIT: This file is NOT managed by fleek. 
-
+  programs.zsh.initExtraBeforeCompInit = ''
+    fpath+=("/usr/share/zsh/site-functions /usr/share/zsh/$ZSH_VERSION/functions /usr/share/zsh/vendor-completions)
+  '';
  
 }
