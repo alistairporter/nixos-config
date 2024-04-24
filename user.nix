8576819a2cbe4f3239ccc6128f25903a77c02eb5
Alistair Porter <alistair_porter@outlook.com>
2024-04-24 21:56:06 +0100

    fleek: apply

--- user.nix
+++ user.nix
@@ -100,8 +100,11 @@
       settings = {
         sync_address = "https://atuin.aporter.xyz";
         sync_frequency = "0";
+        dialect = "uk";
+        secrets_filter = true;
         search_mode = "fulltext";
-        filter_mode_shell_up_key_binding = "session";
+        filter_mode = "global";
+        filter_mode_shell_up_key_binding = "host";
         enter_accept = false;
       };
     };
