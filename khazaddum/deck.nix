925b2d76b8fad542ed96aa3d615f4c35e8956de9
Alistair Porter <alistair_porter@outlook.com>
2024-05-14 20:35:11 +0100

    fleek: apply

--- khazaddum/deck.nix
+++ khazaddum/deck.nix
@@ -2,4 +2,28 @@
   # DO NOT EDIT: This file is managed by fleek. Manual changes will be overwritten.
     home.username = "deck";
     home.homeDirectory = "/home/deck";
+    programs.git = {
+        enable = true;
+        aliases = {
+            pushall = "!git remote | xargs -L1 git push --all";
+            graph = "log --decorate --oneline --graph";
+            add-nowhitespace = "!git diff -U0 -w --no-color | git apply --cached --ignore-whitespace --unidiff-zero -";
+        };
+        userName = "Alistair Porter";
+        userEmail = "alistair@aporter.xyz";
+        extraConfig = {
+            feature.manyFiles = true;
+            init.defaultBranch = "main";
+            gpg.format = "ssh";
+        };
+
+        signing = {
+            key = "~/.ssh/id_rsa";
+            signByDefault = builtins.stringLength "~/.ssh/id_rsa" > 0;
+        };
+
+        lfs.enable = true;
+        ignores = [ ".direnv" "result" ];
+  };
+  
 }
