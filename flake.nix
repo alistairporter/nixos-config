9bc6893ed11d635351acf001ff1fdd0153b57dcb
Alistair Porter <alistair_porter@outlook.com>
2024-08-29 22:59:26 +0100

    flakes!

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "My nixos systems in a flake!";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.05";
+
+    # use the following for unstable:
+    # nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    # or any branch you want:
+    # nixpkgs.url = "nixpkgs/{BRANCH-NAME}";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      lib = nixpkgs.lib;
+    in {
+      nixosConfigurations = {
+        borealis = lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [ ./configuration.nix ];
+      };
+    };
+  };
+}
