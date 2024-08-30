d5314d57498d42aa02ccb0f68681b4d4df728e80
Alistair Porter <alistair_porter@outlook.com>
2024-08-30 21:48:20 +0100

    add ssh jumps

--- programs.nix
+++ programs.nix
@@ -69,5 +69,32 @@
         set multibuffer
       '';
     };
+
+    ssh = {
+      enable = true;
+      serverAliveInterval = 100;
+      matchBlocks = {
+        "morpheus.aporter.xyz" = {
+          host = "morpheus.aporter.xyz";
+          hostname = "10.10.10.1";
+          proxyJump = "alistair@aporter.xyz";
+        };
+        "atlantis.aporter.xyz" = {
+          host = "atlantis.aporter.xyz";
+          hostname = "10.10.10.2";
+          proxyJump = "alistair@aporter.xyz";
+        };
+        "borealis.aporter.xyz" = {
+          host = "borealis.aporter.xyz";
+          hostname = "10.10.10.3";
+          proxyJump = "alistair@aporter.xyz";
+        };
+        "olympus.aporter.xyz" = {
+          host = "olympus.aporter.xyz";
+          hostname = "10.10.10.4";
+          proxyJump = "alistair@aporter.xyz";
+        };
+      };
+    };
   };
 }
