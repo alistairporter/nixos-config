6505c1244c3f4a3d28683a5fe49bb843421d65e0
alistair <alistair_porter@outlook.com>
2024-09-03 00:15:22 +0100

    add praclarush wg

--- morpheus/networking.nix
+++ morpheus/networking.nix
@@ -63,6 +63,11 @@
               publicKey = "vXL+6bfS0uM3hpT0LYbj1GkaH+2sMk9+bYR4hydBo1M=";
               allowedIPs = ["10.10.10.4/32"];
             }
+            {
+              name = "praclarush";
+              publicKey = "VN2Qr2ShRbpGW+V8XyFjERjqiTPjytFy0ZeIZBOn7Fw=";
+              allowedIPs = ["10.10.10.5/32"];
+            }
           ];
         };    
         "wgtunnelvpn" = {
@@ -88,9 +93,14 @@
               allowedIPs = ["10.0.0.3/32"];
             }
             {
-              name ="alistairsteamdeck";
+              name = "alistairsteamdeck";
               publicKey = "wBiOzaNwhvTEIzesU2mj9TfsbAPZpr7tTSvMKlPV9kc=";
               allowedIPs = ["10.0.0.4/32"];
+            }
+            {
+              name = "praclarush";
+              publicKey = "QApm0IY7QMXC2/KtdCooCFjXTimYVSe9bGeEUFG3/x0=";
+              allowedIPs = ["10.0.0.5/32"];
             }
           ];
         };
