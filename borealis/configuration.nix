02efa477e45b14e656090b68e4d6a659416117d2
Alistair Porter <alistair_porter@outlook.com>
2024-09-11 20:50:27 +0100

    actually put hass ssh stuff in right sysconfig, dumbass

--- borealis/configuration.nix
+++ borealis/configuration.nix
@@ -102,6 +102,10 @@
   #     tree
   #   ];
   };
+  users.users.hass = {
+    openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDrcQ7vpeANSs2xXEp16XM2ctW5ZmMPa+7vvZIhCfMj2RJ/vrrdHbJkgfnTsU45Z6t/+WClClWvfO0yWKSZUgvfo+g+1YixSUC3ZfHw6rv5U/uGP1Y/0/JBcyiapSKQTYIeqN/6Ic35ehRD/vDFjdJoC4fUfmdpbWMkHlu7ZArhDDzVKjT2r6CgnGwEYzM6dD0dxwAXLJwWxHdEDm/tBwbQNmjZVIPJw6QMTJQwabO4c5uvtAJ4V1BUT0qTLaoxHw/7l6v0AeegpFTiMLHkmFTh01b3wfGoMZmoSSJhhg0oXHkbQ9NcoatrtNtf9Xibjf5GQEpVj7CmfLP7mlmjgZJJtT1QpIoe/Bi7XFHnWi8O06cn6QWTPptMISghyDUbbwROYBCq+tAbWmW7j7g/cXyrBzlCv/lSX1F1WdSf72g/12MwlPPRhV9KCf2bJX4c/9uS6SQJx2LB+hS9C5eFTLyvsSnND84HLbEnxRd0OSPctNFcltkk6aJ6KOXbGSqJgOM= root@atlantis" ];
+    isNormalUser = true;
+  };
   # disable root account
   users.users.root.hashedPassword = "!";
 
@@ -115,6 +119,7 @@
     usbutils
     pciutils
     sops
+    wakeonlan
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
