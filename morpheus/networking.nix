538b14339c2e5cb4457e430a50675ba7e6970561
alistair <alistair_porter@outlook.com>
2024-12-24 14:20:09 +0000

    move wireguard to support ipv6

--- morpheus/networking.nix
+++ morpheus/networking.nix
@@ -40,7 +40,7 @@
         "wgtunnelinfra" = {
 #          privateKey = "SECRET_REDACTED";
           privateKeyFile = config.sops.secrets.wg_privkey_morpheus_infra.path;
-          ips = ["10.10.10.1/24"];
+          ips = ["10.10.10.1/24" "fd00:dead:beef::1/128"];
           listenPort = 51821;
           postSetup = ''
             ${pkgs.iptables}/bin/iptables -A FORWARD -i wgtunnelinfra -o wgtunnelinfra -m conntrack --ctstate NEW -j ACCEPT
@@ -52,27 +52,27 @@
             {
               name = "atlantis";
               publicKey = "tTtVBxVFAfhhb5oJ7+OJIkKyAz632vn5cv6kCRRrnn4=";
-              allowedIPs = ["10.10.10.2/32"];
+              allowedIPs = ["10.10.10.2/32" "fd00:dead:beef::2/128"];
             }
             {
               name = "borealis";
               publicKey = "TjcatBBmpfBskSL6p8eEKkMqtLZ2QpLLo4OUEZP9NTo=";
-              allowedIPs = ["10.10.10.3/32"];
+              allowedIPs = ["10.10.10.3/32" "fd00:dead:beef::3/128"];
             }
             {
               name = "olympus";
               publicKey = "vXL+6bfS0uM3hpT0LYbj1GkaH+2sMk9+bYR4hydBo1M=";
-              allowedIPs = ["10.10.10.4/32"];
+              allowedIPs = ["10.10.10.4/32" "fd00:dead:beef::4/128"];
             }
             {
               name = "praclarush";
               publicKey = "VN2Qr2ShRbpGW+V8XyFjERjqiTPjytFy0ZeIZBOn7Fw=";
-              allowedIPs = ["10.10.10.5/32"];
+              allowedIPs = ["10.10.10.5/32" "fd00:dead:beef::5/128"];
             }
             {
               name = "alistairpixel5";
               publicKey = "6h3sn7Q6+E8DzpW5O7nlUt15qdIgJr+yubWB5Ev6jHo=";
-              allowedIPs = ["10.10.10.6/32"];
+              allowedIPs = ["10.10.10.6/32" "fd00:dead:beef::6/128"];
             }
           ];
         };    
