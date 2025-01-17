cb5046c4648ddb88f44cdca906c6107ae16c4e64
Alistair Porter <alistair_porter@outlook.com>
2025-01-17 23:04:09 +0000

    borealis: enable avahi

---
+++ borealis/services/avahi.nix
@@ -0,0 +1,24 @@
+{ config, lib, pkgs, ... }:
+
+{
+  services.avahi = {
+    enable = true;
+    ipv4 = true;
+    ipv6 = true;
+    nssmdns4 = true;
+    nssmdns6 = true;
+    openFirewall = true;
+    allowInterfaces = [ "enp0s31f6" "lo" ];
+    extraServiceFiles = {
+      ssh = "${pkgs.avahi}/etc/avahi/services/ssh.service";
+    };
+    publish = {
+      enable = true;
+      addresses = true;
+      workstation = true;
+      hinfo = true;
+      domain = true;
+      userServices = true;
+    };
+  };
+}
