369b855326a0e4934ab28ffbb46314cb74daf05c
Alistair Porter <alistair@aporter.xyz>
2025-04-07 20:02:11 +0100

    morpheus: beszel stuff

--- morpheus/monitoring/monitoring.nix
+++ morpheus/monitoring/monitoring.nix
@@ -9,4 +9,21 @@
       enable = true;
     };
   };
+
+  # Beszel Agent:
+  #
+  systemd.services.beszel = {
+    enable = true;
+    description = "Beszel Agent Service";
+    environment = {
+      PORT = "45876";
+    };
+    after = ["network.target"];
+    serviceConfig = {
+      ExecStart = "${pkgs.beszel}/bin/beszel-agent";
+      EnvironmentFile = "${config.sops.secrets.beszel_key_morpheus.path}";
+    };
+
+    wantedBy = [ "multi-user.target" ];
+  };
 }
