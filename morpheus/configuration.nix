7fcbb2bd7827fc412ade9ec851c9cb8e9da95ac4
Alistair Porter <alistair@aporter.xyz>
2025-04-07 20:29:16 +0100

    morpheus: fix sops path

--- morpheus/configuration.nix
+++ morpheus/configuration.nix
@@ -14,7 +14,7 @@
 
   #Secrets
   sops.defaultSopsFile = ../secrets/morpheus.yaml;
-  sops.age.sshKeyPaths = [ /etc/ssh/ssh_host_ed25519_key ];
+  sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
   sops.secrets.wg_privkey_morpheus_infra = {};
   sops.secrets.wg_privkey_morpheus_vpn = {};
   sops.secrets.microsocks_password_morpheus = {};
