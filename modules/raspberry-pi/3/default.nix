3d3b64c59acf8dc586d4273ac862cf0bf97b39e4
Alistair Porter <alistair@aporter.xyz>
2025-06-29 15:50:39 +0100

    add nixos module for rpi 3

---
+++ modules/raspberry-pi/3/default.nix
@@ -0,0 +1,50 @@
+{ lib, pkgs,config, ...}:
+
+{
+  imports = [
+    ./audio.nix
+    ./digi-amp-plus.nix
+    ./pkgs-overlays.nix
+  ];
+
+  boot.kernelPackages = lib.mkDefault pkgs.linuxKernel.packages.linux_rpi3;
+
+  # fix the following error :
+  # modprobe: FATAL: Module ahci not found in directory
+  # https://github.com/NixOS/nixpkgs/issues/154163#issuecomment-1350599022
+  nixpkgs.overlays = [
+    (_final: super: {
+      makeModulesClosure = x:
+        super.makeModulesClosure (x // { allowMissing = true; });
+    })
+  ];
+
+  # https://github.com/NixOS/nixpkgs/blob/b72bde7c4a1f9c9bf1a161f0c267186ce3c6483c/nixos/modules/installer/sd-card/sd-image-aarch64.nix#L12
+  # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
+  boot.loader.grub.enable = lib.mkDefault false;
+  # Enables the generation of /boot/extlinux/extlinux.conf
+  boot.loader.generic-extlinux-compatible.enable = lib.mkDefault true;
+
+  hardware.deviceTree.filter = lib.mkDefault "bcm2710-rpi-*.dtb";
+
+
+  assertions = [
+    {
+      assertion = (lib.versionAtLeast config.boot.kernelPackages.kernel.version "6.1");
+      message = "This version of raspberry pi 3 dts overlays requires a newer kernel version (>=6.1). Please upgrade nixpkgs for this system.";
+    }
+  ];
+
+  # Required for wifi firmware
+  hardware.enableRedistributableFirmware = true;
+
+  # The last console argument in the list that linux can find at boot will receive kernel logs.
+  # The serial ports listed here are:
+  # - ttyS0: serial
+  # - tty0: hdmi
+  boot.kernelParams = [
+    "console=ttyS0,115200n8"
+    "console=tty0"
+  ];
+
+}
---
+++ modules/raspberry-pi/3/digi-amp-plus.nix
@@ -0,0 +1,90 @@
+{config, lib, ...}:
+
+let
+  cfg = config.hardware.raspberry-pi."3".digi-amp-plus;
+in
+{
+  options.hardware = {
+    raspberry-pi."3".digi-amp-plus = {
+      enable = lib.mkEnableOption ''
+        support for the IQaudIO DigiAMP+ Hat.
+      '';
+
+      unmuteAmp = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = ''
+          "one-shot" unmute when kernel module first loads.
+        '';
+      };
+
+      autoMuteAmp = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = ''
+          Unmute the amp when an ALSA device is opened by a client. Mute, with a five-second delay when the ALSA device is closed.
+          (Reopening the device within the five-second close window will cancel mute.)
+        '';
+      };
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    hardware.deviceTree = {
+      overlays = [
+        # Adapted from https://github.com/NixOS/nixos-hardware/blob/master/raspberry-pi/4/digi-amp-plus.nix
+        # changes:
+        # - modified top-level "compatible" field from bcm2711 to bcm2710
+        # - s/i2s/i2s_clk_producer/ (name on bcm2710 platform)
+        {
+          name = "iqaudio-digiampplus-overlay";
+          dtsText = ''
+            /dts-v1/;
+            /plugin/;
+
+            / {
+              compatible = "brcm,bcm2710";
+
+              fragment@0 {
+                target = <&i2s_clk_producer>;
+                __overlay__ {
+                  status = "okay";
+                };
+              };
+
+              fragment@1 {
+                target = <&i2c1>;
+                __overlay__ {
+                  #address-cells = <1>;
+                  #size-cells = <0>;
+                  status = "okay";
+
+                  pcm5122@4c {
+                    #sound-dai-cells = <0>;
+                    compatible = "ti,pcm5122";
+                    reg = <0x4c>;
+                    AVDD-supply = <&vdd_3v3_reg>;
+                    DVDD-supply = <&vdd_3v3_reg>;
+                    CPVDD-supply = <&vdd_3v3_reg>;
+                    status = "okay";
+                  };
+                };
+              };
+              
+              fragment@2 {
+                target = <&sound>;
+                iqaudio_dac: __overlay__ {
+                  compatible = "iqaudio,iqaudio-dac";
+                  i2s-controller = <&i2s_clk_producer>;
+                  mute-gpios = <&gpio 22 0>;
+                  status = "okay";
+                  ${lib.optionalString cfg.unmuteAmp "iqaudio-dac,unmute-amp;"}
+                  ${lib.optionalString cfg.autoMuteAmp "iqaudio-dac,auto-mute-amp;"}
+                };
+              };
+            };
+          '';
+        }
+      ];
+    };
+  };
+}
