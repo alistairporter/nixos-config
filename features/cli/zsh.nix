385b4ca606ef42f24585015264c8a3cc93130e24
Alistair Porter <alistair_porter@outlook.com>
2025-07-17 11:28:10 +0100

    shell: zsh: remove oh-my-zsh dependency and cleanup file

--- features/cli/zsh.nix
+++ features/cli/zsh.nix
@@ -1,4 +1,4 @@
-{ pkgs, misc, ... }: {
+{ pkgs, lib, misc, ... }: {
 #
 # zsh for a better shell cause bash is rubbish
 #
@@ -6,27 +6,35 @@
     enable = true;
     enableCompletion = true;
     enableVteIntegration = true;
-    completionInit = "autoload -U compinit && compinit -u";
+    #syntaxHighlighting.enable = true;
+    
+    # Enable the below for profiling zsh's startup speed. Once enabled, get numbers using: zsh -i -l -c 'zprof'
+    # initContent = lib.mkOrder 500 "zmodload zsh/zprof";
+    
+    # completionInit = "autoload -U compinit && compinit -u";
+    completionInit = ''
+      autoload -Uz compinit
+      for dump in ~/.zcompdump(N.mh+24); do
+        compinit
+      done
+      
+      compinit -C
+    '';
+
     profileExtra = ''
       [ -r ~/.nix-profile/etc/profile.d/nix.sh ] && source  ~/.nix-profile/etc/profile.d/nix.sh
       export XCURSOR_PATH=$XCURSOR_PATH:/usr/share/icons:~/.local/share/icons:~/.icons:~/.nix-profile/share/icons
     '';
+
     antidote = {
       enable = true;
       plugins = [
-#       "zsh-users/zsh-syntax-highlighting"
        "zsh-users/zsh-completions"
        "belak/zsh-utils path:completion"
-       "ohmyzsh/ohmyzsh path:lib"
-#       "ohmyzsh/ohmyzsh path:plugins/git"
-#       "ohmyzsh/ohmyzsh path:plugins/lol"
-#       "ohmyzsh/ohmyzsh path:plugins/common-aliases"
-       "ohmyzsh/ohmyzsh path:plugins/sudo"
-#       "ohmyzsh/ohmyzsh path:plugins/tmux"
-#       "ohmyzsh/ohmyzsh path:plugins/git"
-#       "ohmyzsh/ohmyzsh path:plugins/docker"
+       "mhzen/zsh-sudo kind:zsh"
+       # "ohmyzsh/ohmyzsh path:lib"
+       # "ohmyzsh/ohmyzsh path:plugins/sudo"
       ];
     };
-    #syntaxHighlighting.enable = true;
   };
 }
