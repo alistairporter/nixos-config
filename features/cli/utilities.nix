6679d45d8d21ed203aa40e246c9b4ed7f333b9e3
Alistair Porter <alistair_porter@outlook.com>
2025-10-04 00:19:49 +0100

    tmux colour stuff

--- features/cli/utilities.nix
+++ features/cli/utilities.nix
@@ -22,12 +22,13 @@
   programs.tmux = {
     enable = true;
     keyMode = "vi";
-    terminal = "screen-256color";
+    terminal = "tmux-256color";
     extraConfig = ''
       bind-key @ choose-window 'join-pane -h -s "%%"'
       bind  c  new-window      -c "#{pane_current_path}"
       bind  %  split-window -h -c "#{pane_current_path}"
       bind '"' split-window -v -c "#{pane_current_path}"
+      set -as terminal-features ",gnome*:RGB"
     '';
   };
   
