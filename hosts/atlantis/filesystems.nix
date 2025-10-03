cca83ab26a48768c9854372c26b18d3c31725d3a
Alistair Porter <alistair_porter@outlook.com>
2025-10-03 23:21:13 +0100

    atlantis: move disk mountpoint around and prepare for snapraid with parity disk and subvolumes on data disks

--- hosts/atlantis/filesystems.nix
+++ hosts/atlantis/filesystems.nix
@@ -17,33 +17,8 @@
     "/home".options = [ "compress=zstd" ];
     "/nix".options = [ "compress=zstd" "noatime" ];
     "/swap".options = [ "noatime" ];
-    
 
-    "/mnt/data/md4" = {
-      device = "/dev/disk/by-uuid/14b2089d-b806-4b2e-939a-5ec03e560378";
-      options = [
-        "defaults"
-        "autodefrag"
-        "compress=zstd"
-      ];
-    };
-    "/mnt/data/md5" = {
-      device = "/dev/disk/by-uuid/e38ac870-e56f-425b-8b13-fdbd5af2a53a";
-      options = [
-        "defaults"
-        "autodefrag"
-        "compress=zstd"
-      ];
-    };
-    "/mnt/data/md6" = {
-      device = "/dev/disk/by-uuid/8b2336d6-90b1-43c5-b696-3f1784780e42";
-      options = [
-        "defaults"
-        "autodefrag"
-        "compress=zstd"
-      ];
-    };
-
+    ## appdata ssd mount
     "/media/MiscFiles" = {
       device = "/dev/disk/by-uuid/3d4a90c0-9bf1-449b-b855-4b04f5e66e30";
       options = [
@@ -54,9 +29,10 @@
       ];
     };
 
+    ## mergerfs mount
     "/media/Files" = {
       fsType = "fuse.mergerfs";
-      device = "/mnt/data/\*";
+      device = "/mnt/disks/md*";
       options = [
         "allow_other"
         "direct_io"
@@ -66,6 +42,97 @@
         "minfreespace=100G"
         "fsname=mergerfsMediaFiles"
         "x-gvfs-show"
+      ];
+    };
+
+    ## snapraid parity mounts
+    "/mnt/disks/pd1" = {
+      device = "/dev/disk/by-uuid/c812a19a-3805-4351-ba83-9d0d90520bed";
+      options = [
+        "defaults"
+        "autodefrag"
+      ];
+    };
+
+    
+    ## btrfs commands
+    ### mkfs.btrfs /dev/disk/blah
+    ### btrfs subvolume create /mnt/disks/diskX/data
+    ### btrfs subvolume create /mnt/snapct/diskX/snapraidContent
+    ## data disk mounts
+    "/mnt/disks/md1" = {
+      device = "/dev/disk/by-uuid/2fd36c76-3626-42ce-befe-248c7a3f2f7c";
+      options = [
+        "defaults"
+        "autodefrag"
+        "compress=zstd"
+        "subvol=data"
+      ];
+    };
+    "/mnt/snapraidContent/md1" = {
+      device = "/dev/disk/by-uuid/2fd36c76-3626-42ce-befe-248c7a3f2f7c";
+      options = [
+        "defaults"
+        "autodefrag"
+        "compress=zstd"
+        "subvol=snapraidContent"
+      ];
+    };
+    
+    "/mnt/disks/md4" = {
+      device = "/dev/disk/by-uuid/14b2089d-b806-4b2e-939a-5ec03e560378";
+      options = [
+        "defaults"
+        "autodefrag"
+        "compress=zstd"
+        "subvol=data"
+      ];
+    };
+    "/mnt/snapraidContent/md4" = {
+      device = "/dev/disk/by-uuid/14b2089d-b806-4b2e-939a-5ec03e560378";
+      options = [
+        "defaults"
+        "autodefrag"
+        "compress=zstd"
+        "subvol=snapraidContent"
+      ];
+    };
+    
+    "/mnt/disks/md5" = {
+      device = "/dev/disk/by-uuid/e38ac870-e56f-425b-8b13-fdbd5af2a53a";
+      options = [
+        "defaults"
+        "autodefrag"
+        "compress=zstd"
+        "subvol=data"
+      ];
+    };
+    "/mnt/snapraidContent/md5" = {
+      device = "/dev/disk/by-uuid/e38ac870-e56f-425b-8b13-fdbd5af2a53a";
+      options = [
+        "defaults"
+        "autodefrag"
+        "compress=zstd"
+        "subvol=snapraidContent"
+      ];
+    };
+    
+    "/mnt/disks/md6" = {
+      device = "/dev/disk/by-uuid/8b2336d6-90b1-43c5-b696-3f1784780e42";
+      options = [
+        "defaults"
+        "autodefrag"
+        "compress=zstd"
+        "subvol=data"
+      ];
+    };
+    "/mnt/snapraidContent/md6" = {
+      device = "/dev/disk/by-uuid/8b2336d6-90b1-43c5-b696-3f1784780e42";
+      options = [
+        "defaults"
+        "autodefrag"
+        "compress=zstd"
+        "subvol=snapraidContent"
       ];
     };
   };
