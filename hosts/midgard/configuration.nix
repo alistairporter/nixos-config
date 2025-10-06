4bcde1f1dd89cce23df39ddb517e6dce0bd598b1
Alistair Porter <alistair_porter@outlook.com>
2025-10-06 16:22:20 +0100

    more networking stuff

--- hosts/midgard/configuration.nix
+++ hosts/midgard/configuration.nix
@@ -43,6 +43,16 @@
   # Configure network connections interactively with nmcli or nmtui.
   networking.networkmanager.enable = true;
 
+  # systemd.network.enable = true;
+
+  services.resolved = {
+    enable = true;
+    # dnssec = "true";
+    # domains = [ "~." ];
+    # fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+    # dnsovertls = "true";
+  };
+
   services.tailscale.enable = true;
 
   # Set your time zone.
