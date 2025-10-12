ed51cb6e83d26fd2fe7a242075e10620b37f5ac0
Alistair Porter <alistair_porter@outlook.com>
2025-10-12 14:58:06 +0100

    sync olympus home-manager changes

--- home-manager/hosts/olympus.nix
+++ home-manager/hosts/olympus.nix
@@ -7,4 +7,9 @@
     fpath+=(/usr/share/zsh/site-functions /usr/share/zsh/$ZSH_VERSION/functions /usr/share/zsh/vendor-completions)
   '';
   
+  programs.zsh.initExtra = ''
+    any-nix-shell zsh --info-right | source /dev/stdin
+  '';
+  
+  home.packages = [ pkgs.sops pkgs.any-nix-shell ];  
 }
