ddaa9ce9500432d1d6d77da4db3038027b709de9
Alistair Porter <alistair_porter@outlook.com>
2025-10-20 23:16:54 +0100

    add more libraries

--- hosts/midgard/configuration.nix
+++ hosts/midgard/configuration.nix
@@ -119,6 +119,37 @@
     nerd-fonts.fira-mono
   ];
 
+  # provide comapibility environment for precompile binaries without witing nixpkgs declarations for them each time.
+  programs.nix-ld = {                                                                                                                                                             
+    enable = true;                                                                                                                                                                
+    libraries = with pkgs;                                                                                                                                                        
+      [                                                                                                                                                                           
+        zlib                                                                                                                                                                      
+        zstd                                                                                                                                                                      
+        stdenv.cc.cc                                                                                                                                                              
+        stdenv.cc.cc.lib                                                                                                                                                          
+        udev                                                                                                                                                                      
+        dbus                                                                                                                                                                      
+        mesa                                                                                                                                                                      
+        libglvnd
+        libgbm
+        vulkan-headers
+        vulkan-loader
+        vulkan-validation-layers
+        curl                                                                                                                                                                      
+        openssl                                                                                                                                                                   
+        attr                                                                                                                                                                      
+        libssh                                                                                                                                                                    
+        bzip2                                                                                                                                                                     
+        libxml2                                                                                                                                                                   
+        acl                                                                                                                                                                       
+        libsodium
+        util-linux
+        xz
+        systemd
+      ];
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
