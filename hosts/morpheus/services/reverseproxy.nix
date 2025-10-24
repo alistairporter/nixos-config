bf2f25a62fbbae787a2536994e3f787605bd719b
Alistair Porter <alistair@aporter.xyz>
2025-10-24 19:20:11 +0100

    morpheus: use tailscale hosts instead of wg ips

--- hosts/morpheus/services/reverseproxy.nix
+++ hosts/morpheus/services/reverseproxy.nix
@@ -32,16 +32,19 @@
           default_backend forgejossh
 
         backend forgejossh
-          server atlantisv4 10.10.10.2:2222
+          # server atlantisv4 10.10.10.2:2222
+          server atlantisv4 atlantis.dropbear-monster.ts.net:2222
 
         backend minecraft
-          server atlantisv4 10.10.10.2:25565 check send-proxy-v2
+          # server atlantisv4 10.10.10.2:25565 check send-proxy-v2
+          server atlantisv4 atlantis.dropbear-monster.ts.net:25565 check send-proxy-v2
 
 #        backend minecraft-voice
 #          server atlantisv4 udp@10.10.10.2:24454 check send-proxy-v2
 
         backend wireguard
-          server atlantisv4 10.10.10.2:8080 check send-proxy-v2
+          # server atlantisv4 10.10.10.2:8080 check send-proxy-v2
+          server atlantisv4 atlantis.dropbear-monster.ts.net:8080 check send-proxy-v2
       '';
     };
 
@@ -55,17 +58,20 @@
         # minecraft geyser
         server {
           listen 19132 udp;
-          proxy_pass 10.10.10.2:19132;
+          # proxy_pass 10.10.10.2:19132;
+          proxy_pass atlantis.dropbear-monster.ts.net:19132;
         }
         # minecraft voice
         server {
           listen 24454 udp;
-          proxy_pass 10.10.10.2:24454;
+          # proxy_pass 10.10.10.2:24454;
+          proxy_pass atlantis.dropbear-monster.ts.net:24454;
         }
         # minecraft voice 2
         server {
           listen 24455 udp;
-          proxy_pass 10.10.10.2:24455;
+          # proxy_pass 10.10.10.2:24455;
+          proxy_pass atlantis.dropbear-monster.ts.net:24455;
         }
       '';
     };
