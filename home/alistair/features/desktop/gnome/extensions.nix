fbc6d1e9d42c75571f05fe2bd0fb22a9585bdac3
Alistair Porter <alistair_porter@outlook.com>
2025-11-04 20:15:27 +0000

    add more gnome extensions and their dconf configs for my setup

--- home/alistair/features/desktop/gnome/extensions.nix
+++ home/alistair/features/desktop/gnome/extensions.nix
@@ -13,8 +13,10 @@
       dash-to-dock
       freon
       fuzzy-app-search
+      gamemode-shell-extension
       gsconnect
       just-perfection
+      logo-menu
       quick-settings-audio-panel
       quick-touchpad-toggle
       tactile
@@ -33,8 +35,10 @@
       dash-to-dock.extensionUuid
       freon.extensionUuid
       fuzzy-app-search.extensionUuid
+      gamemode-shell-extension.extensionUuid
       gsconnect.extensionUuid
       just-perfection.extensionUuid
+      logo-menu.extensionUuid
       quick-settings-audio-panel.extensionUuid
       quick-touchpad-toggle.extensionUuid
       tactile.extensionUuid
@@ -42,5 +46,47 @@
       user-themes.extensionUuid
       user-avatar-in-quick-settings.extensionUuid
     ];
+  };
+
+  dconf.settings."org/gnome/shell/extensions/Logo-menu" = {
+    menu-button-icon-click-type = 2;
+    menu-button-icon-image = 23;
+    menu-button-software-center = "flatpak run io.github.kolunmi.Bazaar";
+    menu-button-system-monitor = "missioncenter";
+    menu-button-terminal = "xdg-terminal-exec";
+    show-activities-button = true;
+    symbolic-icon = true;
+    use-custom-icon = false;
+  };
+
+  dconf.settings."org/gnome/shell/extensions/blur-my-shell/dash-to-dock" = {
+    blur = false;
+  };
+
+  dconf.settings."org/gnome/shell/extensions/blur-my-shell/panel" = {
+    blur = false;
+  };
+  
+  dconf.settings."org/gnome/shell/extensions/dash-to-dock" = {
+    apply-custom-theme = true;
+    custom-theme-shrink = true;
+    dash-max-icon-size = 64;
+    disable-overview-on-startup = false;
+    dock-position = "BOTTOM";
+    require-pressure-to-show = true;
+    show-mounts = false;
+    show-show-apps-button = false;
+    show-trash = false;
+  };
+
+  dconf.settings."org/gnome/shell/extensions/freon" = {
+    show-decimal-value = false;
+    show-icon-on-panel = false;
+    show-rotationrate-unit = true;
+  };
+
+  dconf.settings."org/gnome/shell/extensions/search-light" = {
+    shortcut-search = [ "<Super>space" ];
+    popup-at-cursor-monitor = true;
   };
 }
