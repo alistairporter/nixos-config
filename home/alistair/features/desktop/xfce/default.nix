6d26c2069103e3ad0044ab5602afc773434d2766
Alistair Porter <alistair_porter@outlook.com>
2025-11-04 21:37:27 +0000

    add initial xfce configuration

--- home/alistair/features/desktop/xfce/default.nix
+++ home/alistair/features/desktop/xfce/default.nix
@@ -2,6 +2,25 @@
   imports = [
     ../common
   ];
+  
+  home.packages = with pkgs; [
+    adw-xfwm4
+    xfce.xfce4-icon-theme
+    xfce.xfdesktop
+  ];
+
+  xfconf.settings = {
+    xfwm4 = {
+      "/general/theme" = "adw-gtk3-dark";
+      "/general/activate_action" = "none";
+    };
+
+    xfce4-desktop = {
+      "/desktop-icons/file-icons/show-unknown-removable" = false;
+      "/desktop-icons/file-icons/show-device-fixed" = false;
+    };
+  };
+
   # Enable xdg-desktop-portal and GTK backend
   xdg.portal = {
     enable = true;
@@ -13,5 +32,6 @@
     config = {
       common.default = [ "xapp" "gtk" ];
     };
+
   };
 }
