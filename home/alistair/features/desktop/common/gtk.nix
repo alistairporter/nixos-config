4125b3395754f532321f768201672190f8c91ef7
Alistair Porter <alistair_porter@outlook.com>
2025-11-05 02:20:59 +0000

    add cursor and icon themes, and improve integeration of qt apps in gtk desktops

--- home/alistair/features/desktop/common/gtk.nix
+++ home/alistair/features/desktop/common/gtk.nix
@@ -13,19 +13,51 @@
       name = "adw-gtk3-dark";
       package = pkgs.adw-gtk3;
     };
+    cursorTheme = {
+      name = "Posy_Cursor_Black";
+      package = pkgs.posy-cursors;
+    };
+    iconTheme = {
+      name = "MoreWaita";
+      package = pkgs.morewaita-icon-theme;
+    };
+    gtk3 = {
+      extraConfig.gtk-application-prefer-dark-theme = true;
+    };
+  };
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    hyprcursor.enable = true;
+    x11.enable = true;
+    name = "Posy_Cursor_Black";
+    package = pkgs.posy-cursors;
+    size = 32;
   };
 
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
       gtk-theme = "${config.gtk.theme.name}";
+      cursor-size = config.home.pointerCursor.size;
     };
   };
 
   qt = {
     enable = true;
-    platformTheme.name = "gtk";
+    platformTheme.name = "adwaita";
+    style.name = "adwaita-dark";
   };
 
+  home.sessionVariables = {
+    # For qadwaitadecorations
+    QT_WAYLAND_DECORATION = "adwaita";    
+  };
+
+  home.packages = with pkgs; [
+    # Fix Qt app client decorations
+    qadwaitadecorations
+    qadwaitadecorations-qt6
+  ];
   xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
 }
