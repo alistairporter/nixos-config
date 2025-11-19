3a749618d7411694131f7851b3ae4f370b412731
Alistair Porter <alistair_porter@outlook.com>
2025-11-19 14:34:36 +0000

    add all storage drives, plus reword some instructions

--- hosts/atlantis/filesystems.nix
+++ hosts/atlantis/filesystems.nix
@@ -16,33 +16,51 @@
     enable = true;
     parityFiles = [
       "/mnt/disks/pd1/snapraid.parity"
-      "/mnt/disks/pd2/snapraid.parity"
     ];
     contentFiles = [
       "/var/snapraid.content"
       "/mnt/snapraidContent/md1/snapraid.content"
-      # "/mnt/snapraidContent/md2/snapraid.content"
-      # "/mnt/snapraidContent/md3/snapraid.content"
+      "/mnt/snapraidContent/md2/snapraid.content"
+      "/mnt/snapraidContent/md3/snapraid.content"
       "/mnt/snapraidContent/md4/snapraid.content"
       "/mnt/snapraidContent/md5/snapraid.content"
       "/mnt/snapraidContent/md6/snapraid.content"
+      "/mnt/snapraidContent/md7/snapraid.content"
     ];
     dataDisks = {
       d1 = "/mnt/disks/md1";
-      # d2 = "/mnt/disks/md2";
-      # d3 = "/mnt/disks/md3";
+      d2 = "/mnt/disks/md2";
+      d3 = "/mnt/disks/md3";
       d4 = "/mnt/disks/md4";
       d5 = "/mnt/disks/md5";
       d6 = "/mnt/disks/md6";
+      d7 = "/mnt/disks/md7";
     };
     exclude = [
-      "*.unrecoverable"
-      "/tmp/"
-      "/lost+found/"
       "downloads/"
       "appdata/"
       "*.!sync"
       "/.snapshots/"
+      ".trash/"
+      "*.nfo"
+      "active/"
+      "completed/"
+      "watch/"
+      ".transcode_cache/"
+      "*.unrecoverable"
+      "catcam/"
+      "tmp/"
+      "/lost+found/"
+      ".AppleDouble"
+      "._AppleDouble"
+      ".DS_Store"
+      ".Thumbs.db"
+      ".fseventsd"
+      ".Spotlight-V100"
+      ".TemporaryItems"
+      ".Trashes"
+      ".AppleDB"
+      "._*"
     ];
   };
 
@@ -89,18 +107,19 @@
         "autodefrag"
       ];
     };
-    "/mnt/disks/pd2" = {
-      device = "/dev/disk/by-uuid/83ac0fbd-2869-4958-b5f4-08a71e132545";
-      options = [
-        "defaults"
-        "autodefrag"
-      ];
-    };
 
-    ## btrfs commands
-    ### mkfs.btrfs /dev/disk/blah
-    ### btrfs subvolume create /mnt/disks/diskX/data
-    ### btrfs subvolume create /mnt/snapct/diskX/snapraidContent
+    ### BTRFS commands for setting up new media disk:
+    # format disk with btrfs:
+    # $ sudo mkfs.btrfs /dev/disk/by-uuid/blah
+    ## Then mount somewhere e.g under ~ or /media, where doesn't matter.
+    # $ mount /dev/disk/by-uuid/blah /mnt/driveMountPoint
+    # $ cd /mnt/driveMountPoint/
+    ## Create subvolumes for data and snapraid:
+    # $ btrfs subvolume create data
+    # $ btrfs subvolume create snapraidContent
+    # Finally add mount definition and snapraid confiuration(s), and rebuild the NixOS closure.
+
+     
     ## data disk mounts
     "/mnt/disks/md1" = {
       device = "/dev/disk/by-uuid/2fd36c76-3626-42ce-befe-248c7a3f2f7c";
@@ -113,6 +132,44 @@
     };
     "/mnt/snapraidContent/md1" = {
       device = "/dev/disk/by-uuid/2fd36c76-3626-42ce-befe-248c7a3f2f7c";
+      options = [
+        "defaults"
+        "autodefrag"
+        "compress=zstd"
+        "subvol=snapraidContent"
+      ];
+    };
+    
+    "/mnt/disks/md2" = {
+      device = "/dev/disk/by-uuid/83ac0fbd-2869-4958-b5f4-08a71e132545";
+      options = [
+        "defaults"
+        "autodefrag"
+        "compress=zstd"
+        "subvol=data"
+      ];
+    };
+    "/mnt/snapraidContent/md2" = {
+      device = "/dev/disk/by-uuid/83ac0fbd-2869-4958-b5f4-08a71e132545";
+      options = [
+        "defaults"
+        "autodefrag"
+        "compress=zstd"
+        "subvol=snapraidContent"
+      ];
+    };
+
+    "/mnt/disks/md3" = {
+      device = "/dev/disk/by-uuid/0013e2f5-f54e-4074-9e0e-ecca9acefb3a";
+      options = [
+        "defaults"
+        "autodefrag"
+        "compress=zstd"
+        "subvol=data"
+      ];
+    };
+    "/mnt/snapraidContent/md3" = {
+      device = "/dev/disk/by-uuid/0013e2f5-f54e-4074-9e0e-ecca9acefb3a";
       options = [
         "defaults"
         "autodefrag"
@@ -170,6 +227,25 @@
     };
     "/mnt/snapraidContent/md6" = {
       device = "/dev/disk/by-uuid/8b2336d6-90b1-43c5-b696-3f1784780e42";
+      options = [
+        "defaults"
+        "autodefrag"
+        "compress=zstd"
+        "subvol=snapraidContent"
+      ];
+    };
+    
+    "/mnt/disks/md7" = {
+      device = "/dev/disk/by-uuid/e1fb50b0-7b71-4490-a752-69af36d44c7c";
+      options = [
+        "defaults"
+        "autodefrag"
+        "compress=zstd"
+        "subvol=data"
+      ];
+    };
+    "/mnt/snapraidContent/md7" = {
+      device = "/dev/disk/by-uuid/e1fb50b0-7b71-4490-a752-69af36d44c7c";
       options = [
         "defaults"
         "autodefrag"
