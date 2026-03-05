c1a30bb49db1eb8111e34c69b05db2efd8d4caab
Alistair Porter <alistair_porter@outlook.com>
2026-03-05 12:09:37 +0000

    add printer drivers for hp and gutenprint

--- hosts/common/optional/printing.nix
+++ hosts/common/optional/printing.nix
@@ -1,6 +1,10 @@
 {
+  pkgs,
+  ...
+}: {
   services.printing = {
     enable = true;
+    drivers = with pkgs; [ gutenprint hplip ];
     webInterface = false;
     cups-pdf = {
       enable = true;
