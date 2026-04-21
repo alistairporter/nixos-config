54a220c1f9c9a03aedd3576483bc4d83d6419c67
Alistair Porter <alistair_porter@outlook.com>
2026-04-21 23:18:49 +0100

    all: add systemd-boot entries for usefull tools
    Add netbootxyz, memtest, and an efi shell to all
    machines that are using systemd-boot.

--- hosts/common/optional/systemd-boot.nix
+++ hosts/common/optional/systemd-boot.nix
@@ -3,6 +3,9 @@
     systemd-boot = {
       enable = true;
       consoleMode = "max";
+      netbootxyz.enable = true;
+      edk2-uefi-shell.enable = true;
+      memtest86.enable = true;
     };
     efi.canTouchEfiVariables = true;
   };
