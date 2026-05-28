b4d3bb11e0a98bc6f77b78b229d98bb0acc92a21
Alistair Porter <alistair_porter@outlook.com>
2026-05-28 23:20:55 +0100

    update flake deps, plus patch out broken pipx tests

--- hosts/common/global/packages.nix
+++ hosts/common/global/packages.nix
@@ -12,7 +12,15 @@
     lm_sensors
     nano
     pciutils
-    pipx
+    # See https://github.com/NixOS/nixpkgs/issues/522307
+    (pipx.overridePythonAttrs (old: {
+      disabledTests =
+        (old.disabledTests or [])
+        ++ [
+          "test_fix_package_name"
+          "test_parse_specifier_for_metadata"
+        ];
+    }))
     progress
     python3
     smartmontools
