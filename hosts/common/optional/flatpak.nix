1a45822a04610d6b2f805125ee71425ed38c9828
Alistair Porter <alistair_porter@outlook.com>
2026-09-04 05:09:25 +0100

    disable broken flatpak repo service

--- hosts/common/optional/flatpak.nix
+++ hosts/common/optional/flatpak.nix
@@ -6,11 +6,11 @@
 }: {
   # Enable flatpak and flathub
   services.flatpak.enable = true;
-  systemd.services.flatpak-repo = {
-    wantedBy = ["multi-user.target"];
-    path = [pkgs.flatpak];
-    script = ''
-      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    '';
-  };
+  # systemd.services.flatpak-repo = {
+  #   wantedBy = ["multi-user.target"];
+  #   path = [pkgs.flatpak];
+  #   script = ''
+  #     flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+  #   '';
+  # };
 }
